fe6d6d9d736ab22178ef952a3e2c97eabb9ad193
Manjula36 <manjulavdt@gmail.com>
2024-02-15 12:13:15 +0530

    final Commit

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using WelcomeApp;
+
+Console.WriteLine("welcome to Authentication App");
+Signin obj = new Signin();
+Console.WriteLine(obj.Authentication("admin","admin@123"));
---
+++ Signin.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WelcomeApp
+{
+    public class Signin
+    {
+        public string Authentication(string userName, string password)
+        {
+            string msg;
+            if ((string.IsNullOrEmpty(userName)) || string.IsNullOrEmpty(password))
+            {
+                msg = "User name and password requiredd";
+            }
+            else
+            {
+                if ((userName == "admin") && (password == "admin@123"))
+                {  
+                    msg = "Authentication Pass";
+                }
+                 else
+                {
+                    msg = "Authentication Fail";
+                }
+            }
+        return msg;
+        }
+}
+}
